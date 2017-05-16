19a3d1a647fc0a1d65feecf2bda6402f767df801
Silence <374619540@qq.com>
2017-05-16 11:27:08 +0800

    Update

--- MetaManagerDemo/MetaManager/SIMediaItem.m
+++ MetaManagerDemo/MetaManager/SIMediaItem.m
@@ -20,26 +20,26 @@
 @implementation AVMetadataItem (KeyString)
 
 - (NSString *)keyString {
-    if ([self.key isKindOfClass:[NSString class]]) {                        // 1
+    if ([self.key isKindOfClass:[NSString class]]) {
         return (NSString *)self.key;
     }
     else if ([self.key isKindOfClass:[NSNumber class]]) {
-        UInt32 keyValue = [(NSNumber *) self.key unsignedIntValue];         // 2
-        size_t length = sizeof(UInt32);                                     // 3
+        UInt32 keyValue = [(NSNumber *) self.key unsignedIntValue];
+        size_t length = sizeof(UInt32);
         if ((keyValue >> 24) == 0) --length;
         if ((keyValue >> 16) == 0) --length;
         if ((keyValue >> 8) == 0) --length;
         if ((keyValue >> 0) == 0) --length;
         long address = (unsigned long)&keyValue;
         address += (sizeof(UInt32) - length);
-        keyValue = CFSwapInt32BigToHost(keyValue);                          // 4
-        char cstring[length];                                               // 5
+        keyValue = CFSwapInt32BigToHost(keyValue);
+        char cstring[length];
         strncpy(cstring, (char *) address, length);
         cstring[length] = '\0';
-        if (cstring[0] == '\xA9') {                                         // 6
+        if (cstring[0] == '\xA9') {
             cstring[0] = '@';
         }
-        return [NSString stringWithCString:(char *) cstring                 // 7
+        return [NSString stringWithCString:(char *) cstring                 
                                   encoding:NSUTF8StringEncoding];
         
     }
